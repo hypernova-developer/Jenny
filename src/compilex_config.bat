84ce3ed4cf404e72857bfda404c6713e0c4ce3bb
卄ㄚ卩乇尺几ㄖᐯ卂 <mhmmdeymngrbz@gmail.com>
2026-04-12 20:58:25 +0300

    Delete src/compilex_config.bat

--- src/compilex_config.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "GCC_PATH=C:\mingw64\bin\gcc.exe"
-set "GXX_PATH=C:\mingw64\bin\g++.exe"
-set "GFORTRAN_PATH=C:\mingw64\bin\gfortran.exe"
-set "CSC_PATH=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
-set "PYTHON_PATH=C:\Users\Username\AppData\Local\Programs\Python\Python312\python.exe"
-set "SYNTAX_INCLUDE=E:\Developing\hypernova-developer\SyntaX\Fortran"
